cfc5adfc3cf0cf4d01d403a271022c0fcbc5ec9b
DEVprasad7 <prasad.maskar7@gmail.com>
2025-12-08 21:07:18 +0530

    Changing URL for Server

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI(
 # CORS configuration for Next.js frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://blockchain-backend-seven.vercel.app"], # Earlier http://localhost:3000
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
